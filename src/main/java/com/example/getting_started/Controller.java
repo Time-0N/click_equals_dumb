aa10cfe29539f30b142e324bd3d9f2a5ff146c8c
Time_ON <haas.timeon@gmail.com>
2023-12-05 15:21:42 +0100

    Message list added Funktion added add Message list Funktion added delete Message list

--- src/main/java/com/example/getting_started/Controller.java
+++ src/main/java/com/example/getting_started/Controller.java
@@ -1,11 +1,38 @@
 package com.example.getting_started;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 public class Controller {
+
+   ArrayList<Message> messageList = new ArrayList<>();
+
+   @DeleteMapping("/messages/{id}")
+   public List<Message> deleteFromList(@PathVariable String id) {
+
+       for (Message m : messageList) {
+           if (m.id().equals(id)) {
+               messageList.remove(m);
+               return messageList;
+           }
+       }
+       return messageList;
+   }
+
+   @GetMapping("/messages")
+   public List<Message> printList() {
+       return messageList;
+   }
+
+   @PostMapping("/messages")
+   public Message addList(@RequestBody Message message) {
+       messageList.add(message);
+       return message;
+   }
 
     @GetMapping("/hello")
     public String helloWolrd() {
---
+++ src/main/java/com/example/getting_started/Message.java
@@ -0,0 +1,8 @@
+package com.example.getting_started;
+
+public record Message(
+        String name,
+        String message,
+        String id
+) {
+}
