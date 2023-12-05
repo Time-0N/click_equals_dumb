0fe9f5ab95f19c90fd0bd9977916779a67e68e2c
Time_ON <haas.timeon@gmail.com>
2023-12-05 14:49:10 +0100

    bug fix

--- src/main/java/com/example/getting_started/Controller.java
+++ src/main/java/com/example/getting_started/Controller.java
@@ -1,2 +1,19 @@
-package com.example.getting_started;public class Controller {
+package com.example.getting_started;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String helloWolrd() {
+        return "Hello world";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable String name) {
+        return "Hello, " + name + "!";
+    }
 }
